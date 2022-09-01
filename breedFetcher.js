f7ad29c7508fd06cca1f47424fdaa3037bdef483
Cindy <cindy.c0106@gmail.com>
2022-09-01 21:54:45 +0000

    adding test file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,37 @@
 const request = require('request');
-const fs = require('fs');
-
-const breed = process.argv[2];
-url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-
-
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-    return;
-  }
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-  //console.log(typeof data);
-});
 
 
 
 
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null); // Print the error if one occurred
+      return;
+    }
 
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback('Breed not found', null);
+      return;
+
+    }
+
+    callback(null, data[0].description);
+
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
+// request(url, (error, response, body) => {
+//   if (error) {
+//     console.log('error:', error); // Print the error if one occurred
+//     return;
+//   }
+//   const data = JSON.parse(body);
+//   console.log(data[0].description);
+//   //console.log(typeof data);
+// });
