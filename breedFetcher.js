2057e676c3bca24662fa8eb69d2e2e51a944cd18
Cindy <cindy.c0106@gmail.com>
2022-09-01 20:45:05 +0000

    adding breedFetcher.js

---
+++ breedFetcher.js
@@ -0,0 +1,39 @@
+const request = require('request');
+const fs = require('fs');
+
+const breed = process.argv[2];
+url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log('error:', error); // Print the error if one occurred
+    return;
+  }
+  const data = JSON.parse(body);
+  console.log(data[0].description);
+  //console.log(typeof data);
+});
+
+
+
+
+
+
+
+
+// request(url, (error, response, body) => {
+//   if (error) {
+//     console.log('error:', error); // Print the error if one occurred
+//     return;
+//   }
+
+//   fs.writeFile(filePath, body, err => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//   });
+
+// });
